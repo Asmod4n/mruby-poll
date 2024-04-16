e91c75ec8ba44b45e796eeacdf3422dc96198bcb
Asmod4n <791770+Asmod4n@users.noreply.github.com>
2024-04-16 12:46:44 +0200

    add remove_update_or_add method

--- mrblib/poll.rb
+++ mrblib/poll.rb
@@ -1,3 +1,18 @@
 class Poll
   attr_reader :fds
+
+  def remove_update_or_add(socket, pollmask)
+    fds.each do |pfd|
+      if pfd.socket == socket
+        if pollmask == 0
+          remove(pfd)
+          return nil
+        else
+          pfd.events = pollmask
+          return pfd
+        end
+      end
+    end
+    add(socket, pollmask)
+  end
 end
